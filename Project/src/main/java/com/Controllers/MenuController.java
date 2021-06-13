09e0813105ebb16386778ba57fd10809ef95b0d4
moddbno <18130092@st.hcmuaf.edu.vn>
2021-06-13 09:00:15 +0700

    fix

---
+++ Project/src/main/java/com/Controllers/MenuController.java
@@ -0,0 +1,31 @@
+package com.Controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MenuController", value = "/MenuController")
+public class MenuController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    //
+    public void tiepNhanBenhNhan(){
+
+    }
+
+    public void khamBenh(){
+
+    }
+    public void thanhToan(){
+
+    }
+}
